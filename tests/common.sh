4c8315c7557e3c5a56c300bc4b32dd9938680d95
Vincent <4919840+vincent-octo@users.noreply.github.com>
2025-06-02 12:45:54 +0300

    maint: improve Bash test runner

--- tests/common.sh
+++ tests/common.sh
@@ -1,3 +1,12 @@
+function log ()
+{
+   printf "\n\n"
+   printf "\e[93m--~*~--\e[m "
+   printf "\e[30;103m $@ \e[m"
+   printf " \e[93m--~*~--\e[m "
+   printf "\n"
+}
+
 function build_mmpio ()
 {
     go build -C ../../src -v -o ../mmpio
